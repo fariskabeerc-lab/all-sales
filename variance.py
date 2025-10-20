1c4d88fc4c8b2299dfa1f134d9dc4d70c15f77bd
fariskabeerc-lab <fariskabeerc@gmail.com>
2025-10-20 09:55:48 +0400

    Update variance.py

--- variance.py
+++ variance.py
@@ -19,9 +19,10 @@ OUTLET_FILES = {
     "Sabah": "sabah oct.Xlsx",
     "Sahat": "sahat oct.Xlsx",
     "Shams HM": "shams HM oct.Xlsx",
-    "Shams LLC": "shams llc oct.Xlsx",
+    "Shams Liwan": "shams llc oct.Xlsx",
     "Superstore": "superstore oct.Xlsx",
-    "Tay Tay": "tay tay oct.Xlsx"
+    "Tay Tay": "tay tay oct.Xlsx",
+    "Safa oudmehta": "oudmehta oct.Xlsx"
 }
 
 # ===============================
